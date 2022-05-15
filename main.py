6f49bc406baf5ee28c0426dfaf7341ae99d05ccc
ThatLocalIdiot <105609995+ThatLocalIdiot@users.noreply.github.com>
2022-05-15 23:32:26 +0000

    v0.0.2 - Testing out randomness. Added Random thingy to generate 10 things for a 3d space, and just as I type this I realize that what I have isn't how 3d spaces work. I have just what thing in each x, y, and z index. Meaning it really isn't 3d. Also need to figure out how to generate a list of all the things nearby to something, and decide what to do somehow.

--- main.py
+++ main.py
@@ -1,22 +1,173 @@
+import random
 upQuark = 1
 downQuark = 0
 electron = -1
 x = []
 y = []
 z = []
+index = 0
 
-
-def checkMovement(i):
+def checkMovement(i, list):
+  if i == upQuark:
+    if 0 <= index+1 < len(x):
+      if x[index+1] == upQuark:
+        print(list[index], x[index+1])
+      if x[index-1] == upQuark:
+        print(list[index], x[index+1])
+    if 0 <= index+1 < len(y):
+      if y[index+1] == upQuark:
+        print(list[index], y[index+1])
+      if y[index-1] == upQuark:
+        print(list[index], y[index-1])
+    if 0 <= index+1 < len(z):
+      if z[index+1] == upQuark:
+        print(list[index], z[index+1])
+      if z[index-1] == upQuark:
+        print(list[index], z[index-1])
+    if 0 <= index+1 < len(x):
+      if x[index+1] == downQuark:
+        print(list[index], x[index+1])
+      if x[index-1] == downQuark:
+        print(list[index], x[index+1])
+    if 0 <= index+1 < len(y):
+      if y[index+1] == downQuark:
+        print(list[index], y[index+1])
+      if y[index-1] == downQuark:
+        print(list[index], y[index-1])
+    if 0 <= index+1 < len(z):
+      if z[index+1] == downQuark:
+        print(list[index], z[index+1])
+      if z[index-1] == downQuark:
+        print(list[index], z[index-1])
+    if 0 <= index+1 < len(x):
+      if x[index+1] == electron:
+        print(list[index], x[index+1])
+      if x[index-1] == electron:
+        print(list[index], x[index+1])
+    if 0 <= index+1 < len(y):
+      if y[index+1] == electron:
+        print(list[index], y[index+1])
+      if y[index-1] == electron:
+        print(x[index], y[index-1])
+    if 0 <= index+1 < len(z):
+      if z[index+1] == electron:
+        print(list[index], z[index+1])
+      if z[index-1] == electron:
+        print(list[index], z[index-1])
+  if i == downQuark:
+    if 0 <= index+1 < len(x):
+      if x[index+1] == upQuark:
+        print(list[index], x[index+1])
+      if x[index-1] == upQuark:
+        print(list[index], x[index+1])
+    if 0 <= index+1 < len(y):
+      if y[index+1] == upQuark:
+        print(list[index], y[index+1])
+      if y[index-1] == upQuark:
+        print(list[index], y[index-1])
+    if 0 <= index+1 < len(z):
+      if z[index+1] == upQuark:
+        print(list[index], z[index+1])
+      if z[index-1] == upQuark:
+        print(list[index], z[index-1])
+    if 0 <= index+1 < len(x):
+      if x[index+1] == downQuark:
+        print(list[index], x[index+1])
+      if x[index-1] == downQuark:
+        print(list[index], x[index+1])
+    if 0 <= index+1 < len(y):
+      if y[index+1] == downQuark:
+        print(list[index], y[index+1])
+      if y[index-1] == downQuark:
+        print(list[index], y[index-1])
+    if 0 <= index+1 < len(z):
+      if z[index+1] == downQuark:
+        print(list[index], z[index+1])
+      if z[index-1] == downQuark:
+        print(list[index], z[index-1])
+    if 0 <= index+1 < len(x):
+      if x[index+1] == electron:
+        print(list[index], x[index+1])
+      if x[index-1] == electron:
+        print(list[index], x[index+1])
+    if 0 <= index+1 < len(y):
+      if y[index+1] == electron:
+        print(list[index], y[index+1])
+      if y[index-1] == electron:
+        print(x[index], y[index-1])
+    if 0 <= index+1 < len(z):
+      if z[index+1] == electron:
+        print(list[index], z[index+1])
+      if z[index-1] == electron:
+        print(list[index], z[index-1]) 
+  if i == electron:
+    if 0 <= index+1 < len(x):
+      if x[index+1] == upQuark:
+        print(list[index], x[index+1])
+      if x[index-1] == upQuark:
+        print(list[index], x[index+1])
+    if 0 <= index+1 < len(y):
+      if y[index+1] == upQuark:
+        print(list[index], y[index+1])
+      if y[index-1] == upQuark:
+        print(list[index], y[index-1])
+    if 0 <= index+1 < len(z):
+      if z[index+1] == upQuark:
+        print(list[index], z[index+1])
+      if z[index-1] == upQuark:
+        print(list[index], z[index-1])
+    if 0 <= index+1 < len(x):
+      if x[index+1] == downQuark:
+        print(list[index], x[index+1])
+      if x[index-1] == downQuark:
+        print(list[index], x[index+1])
+    if 0 <= index+1 < len(y):
+      if y[index+1] == downQuark:
+        print(list[index], y[index+1])
+      if y[index-1] == downQuark:
+        print(list[index], y[index-1])
+    if 0 <= index+1 < len(z):
+      if z[index+1] == downQuark:
+        print(list[index], z[index+1])
+      if z[index-1] == downQuark:
+        print(list[index], z[index-1])
+    if 0 <= index+1 < len(x):
+      if x[index+1] == electron:
+        print(list[index], x[index+1])
+      if x[index-1] == electron:
+        print(list[index], x[index+1])
+    if 0 <= index+1 < len(y):
+      if y[index+1] == electron:
+        print(list[index], y[index+1])
+      if y[index-1] == electron:
+        print(x[index], y[index-1])
+    if 0 <= index+1 < len(z):
+      if z[index+1] == electron:
+        print(list[index], z[index+1])
+      if z[index-1] == electron:
+        print(list[index], z[index-1])
   return
-def updateUniverse(i):
-  checkMovement(i)
+def updateUniverse(i, list):
+  checkMovement(i, list)
+
+for i in range(0, 10):
+  x.append(random.randint(-1, 1))
+  y.append(random.randint(-1, 1))
+  z.append(random.randint(-1, 1))
   
 while True:
   for i in x:
-    updateUniverse(x)
+    updateUniverse(x[index], x)
+    index += 1
+  index = 0
   for i in y:
-    updateUniverse(y)
+    updateUniverse(y[index], y)
+    index += 1
+  index = 0
   for i in z:
-    updateUniverse(z)  
+    updateUniverse(z[index], z)
+    index += 1
+  index = 0
   break
-  
+
+print(x, y, z)
