b35a41f64852d2ba16b2bfe6b5b71e1bddb090fe
ThatLocalIdiot <105609995+ThatLocalIdiot@users.noreply.github.com>
2022-05-15 22:47:48 +0000

    v0.0.1 - Humble Start. Got basic idea down on how everything is going to work, made matter down to the smallest portions that are proven (i think)

---
+++ main.py
@@ -0,0 +1,22 @@
+upQuark = 1
+downQuark = 0
+electron = -1
+x = []
+y = []
+z = []
+
+
+def checkMovement(i):
+  return
+def updateUniverse(i):
+  checkMovement(i)
+  
+while True:
+  for i in x:
+    updateUniverse(x)
+  for i in y:
+    updateUniverse(y)
+  for i in z:
+    updateUniverse(z)  
+  break
+  
